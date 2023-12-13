e5435e5233a6dd953faf96f090a09f17b4bf5c8a
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-13 07:46:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1= 31;
+let num2= 2;
+let multiply= num1*num2;
+//results will be 62
 
-// Write your code here
+
+let random = 7; random =>0;
+
+let num3= 28;
+let num4= 6;
+let divide= num1/num2;
+// results remainder 4
+
+let mod = 15 % 11; // This will result in a remainder of 4
+console.log(mod); // Output: 4
+
+let max= 20;
+console.log(max);
